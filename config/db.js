7c1f75125e71c5651c64104fdbc406f73eccac4f
viettranni <vttranviett@gmail.com>
2024-09-16 14:51:15 +0300

    Week 5: Refactor tours API to use MongoDB

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/web-dev");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
